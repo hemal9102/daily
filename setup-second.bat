62a6c4f8427743b53adb5a04bf2beca1f1ffc9a5
hemal9102 <hemal.shah2004@gmail.com>
2026-05-21 13:15:01 +0530

    update

--- setup-second.bat
+++ setup-second.bat
@@ -5,7 +5,12 @@ echo Configuring repo for second GitHub account...
 git config user.name "hemal9102"
 git config user.email "hemal.shah2004@gmail.com"
 
-git remote set-url origin git@github-second:hemal9102/daily.git
+git remote get-url origin >nul 2>&1
+if errorlevel 1 (
+    git remote add origin git@github-second:hemal9102/daily.git
+) else (
+    git remote set-url origin git@github-second:hemal9102/daily.git
+)
 
 echo.
 echo Repo configured successfully.
